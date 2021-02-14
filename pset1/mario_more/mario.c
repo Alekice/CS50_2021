4496c2a97311e8a432c1af083763d5c6dc147691
Alekice <70897176+Alekice@users.noreply.github.com>
2021-02-14 10:52:01 +0300

    Create mario.c

---
+++ pset1/mario_more/mario.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int rows = 0; rows < height; rows++)
+    {
+        int spaces = (height - 1) - rows;
+        int hashtags = rows + 1;
+
+        for (int i = 0; i < spaces; i++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < hashtags; j++)
+        {
+            printf("#");
+        }
+
+        printf(" ");
+
+        for (int j = 0; j < hashtags; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
